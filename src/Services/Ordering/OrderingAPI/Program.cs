0eee81d63defc82cf977342a44e5e23b25dccfdb
Bert1041 <ishokarobertt@gmail.com>
2023-05-08 15:59:43 +0100

    Register Application and Infrastructure Layer Dependencies into Ordering.API

--- src/Services/Ordering/OrderingAPI/Program.cs
+++ src/Services/Ordering/OrderingAPI/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.Extensions.Configuration;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
 // Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(config);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
